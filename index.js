0ebb09a4a1649d751d58ba5c1567263eed0d2265
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2022-11-13 18:47:16 -0500

    deploy to pages

---
+++ index.js
@@ -0,0 +1,43 @@
+// Your code here
+const URL = `https://api.openweathermap.org/data/2.5/weather`;
+const API_KEY = "8caf522b8811253c0984f6045871eab9";
+const form = document.querySelector("form");
+let weatherEl = document.querySelector("#weather");
+let input = document.querySelector("#weather-search");
+
+form.onsubmit = (e) => {
+	e.preventDefault();
+
+	let locale = input.value.trim();
+	let queryString = `?q=${locale}&units=imperial&appid=${API_KEY}`;
+	let fetchURL = `${URL}${queryString}`;
+
+	fetch(fetchURL)
+		.then((res) => {
+			if (res.status !== 200) {
+				input.value = "";
+				throw new Error(`Location not found`);
+			}
+			input.value = "";
+			weatherEl.innerHTML = "";
+			return res.json();
+		})
+		.then((data) => {
+			let dateTime = data.dt * 1000;
+			let date = new Date(dateTime);
+			let timeString = date.toLocaleTimeString("en-US", {
+				hour: "numeric",
+				minute: "2-digit"
+			});
+			weatherEl.innerHTML = `
+				<h2>City: ${data.name}, ${data.sys.country}</h2> 
+				<a href="https://www.google.com/maps/search/?api=1&query=${data.coord.lat},${data.coord.lon}" target="_blank">Click to view map</a>
+				<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+				<p style="text-transform: capitalize;">Description: ${data.weather[0].description}</p><br>
+				<p>Actual Temp: ${data.main.temp}</p>
+				<p>Perceived: ${data.main.feels_like}</p>
+				<p>Last Updated: ${timeString}</p>
+			`;
+		})
+		.catch((err) => (weatherEl.textContent = err.message));
+};
